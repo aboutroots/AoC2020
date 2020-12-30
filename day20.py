c964c2de585c983b3177513894ed3b7b6d8f3c50
aboutroots <popiel.mw@gmail.com>
2020-12-30 12:28:33 +0100

    WIP can plot dragons

--- day20.py
+++ day20.py
@@ -1,64 +1,32 @@
-import itertools
 import math
 from collections import defaultdict
 
-from common import file_to_lines
 import numpy as np
+import plotly.express as px
 import re
+
+from _plotly_utils.colors import hex_to_rgb
 
 
 class Tile:
     def __init__(self, id, rows):
         self.id = id
         self.rows = rows
-        self.neighbours = {
-            "n": None,
-            "e": None,
-            "s": None,
-            "w": None,
-        }
 
-    @property
-    def has_all_neighbours(self):
-        return all(n is not None for n in self.neighbours.values())
-
-    @property
-    def has_any_neighbours(self):
-        return any(n is not None for n in self.neighbours.values())
-
-    @property
-    def neighbours_count(self):
-        return sum(1 if v is not None else 0 for v in self.neighbours.values())
-
-    def reset_neighbours(self):
-        for direction, n in self.neighbours.items():
-            if n is not None:
-                side = self.get_sides()[direction]
-                other_dir = n.side_match(side)
-                n.neighbours[other_dir] = None
-        self.neighbours = {
-            "n": None,
-            "e": None,
-            "s": None,
-            "w": None,
-        }
+    def rotate90(self):
+        # this rotates COUNTER CLOCKWISE AHHHH SO MUCH PAIN
+        self.rows = np.rot90(self.rows)
 
     def flip_x(self):
-        self.reset_neighbours()
         new_rows = []
         for row in self.rows:
             new_rows.append(list(reversed(row)))
         self.rows = new_rows
 
-    def rotate90(self):
-        self.reset_neighbours()
-        self.rows = np.rot90(self.rows)
-
     def flip_y(self):
-        self.reset_neighbours()
         self.rows = list(reversed(self.rows))
 
-    def get_sides(self):  # n e s w
+    def get_sides(self):
         return {
             "n": self.rows[0],
             "e": [row[-1] for row in self.rows],
@@ -67,43 +35,24 @@ class Tile:
         }
 
     def side_match(self, other_tile_side):
+        other_side_str = "".join(other_tile_side)
         for side_name, side in self.get_sides().items():
-            # print(''.join(side), ''.join(other_tile_side))
-            if ''.join(side) == ''.join(other_tile_side):
-                return side_name, False
-            if ''.join(list(reversed(side))) == ''.join(other_tile_side):
-                return side_name, True
+            side_str = "".join(side)
+            side_str_rev = "".join(reversed(side))
+            if side_str == other_side_str or side_str_rev == other_side_str:
+                return side_name
         return None
 
     def __repr__(self):
-        return f"TILE<{self.id}>"
-
-
-def compute_neighbours(tile, tiles):
-    if not tile.has_all_neighbours:
-        for direction, neighbour in tile.neighbours.items():
-            if neighbour is not None:
-                continue
-            side = tile.get_sides()[direction]
-            for other_tile in tiles:
-                if other_tile == tile or other_tile.has_all_neighbours:
-                    continue
-                side_match = other_tile.side_match(side)
-                if side_match:
-                    tile.neighbours[direction] = other_tile
-                    other_tile.neighbours[side_match] = tile
-                    break
+        return f"<TILE:{self.id}>"
 
 
 def get_edges_count_map(tiles):
     edges = defaultdict(lambda: 0)
     for tile in tiles:
         for side in tile.get_sides().values():
-            edge = "".join(side)
-            edges[edge] += 1
-
-            rev = "".join(reversed(side))
-            edges[rev] += 1
+            edges["".join(side)] += 1
+            edges["".join(reversed(side))] += 1
     return edges
 
 
@@ -111,8 +60,8 @@ def get_corner_tiles(tiles, edges_map):
     corners = []
     for tile in tiles:
         tile_edges = ["".join(side) for side in tile.get_sides().values()]
-        loose = len([edge for edge in tile_edges if edges_map[edge] == 1])
-        if loose == 2:
+        loose_side = len([edge for edge in tile_edges if edges_map[edge] == 1])
+        if loose_side == 2:
             corners.append(tile)
     return corners
 
@@ -123,168 +72,117 @@ def first(tiles):
     return math.prod([c.id for c in corners])
 
 
-def get_tile_right_of(last_tile, tiles):
-    common_edge = last_tile.get_sides()["e"]
-    next_tile = [
-        tile for tile in tiles if tile.side_match(common_edge) and tile != last_tile
-    ][0]
-    direction_in_other, rev = next_tile.side_match(common_edge)
-    # if direction_in_other == 'w' and not rev:
-    #     continue
-    if direction_in_other == "w" and rev:
-        next_tile.flip_y()
-    elif direction_in_other == "e" and not rev:
-        next_tile.flip_x()
-    elif direction_in_other == "e" and rev:
-        next_tile.rotate90()
-        next_tile.rotate90()
-    elif direction_in_other == "n" and not rev:
-        next_tile.flip_x()
-        next_tile.rotate90()
-        next_tile.rotate90()
-        next_tile.rotate90()
-    elif direction_in_other == "n" and rev:
-        next_tile.rotate90()
-        next_tile.rotate90()
-        next_tile.rotate90()
-    elif direction_in_other == "s" and not rev:
-        next_tile.rotate90()
-    elif direction_in_other == "s" and rev:
-        next_tile.flip_x()
-        next_tile.rotate90()
-    return next_tile
-
-
-def get_tile_down_of(last_tile, tiles):
-    common_edge = last_tile.get_sides()["s"]
-    next_tile = [
-        tile for tile in tiles if tile.side_match(common_edge) and tile != last_tile
-    ][0]
-    direction_in_other, rev = next_tile.side_match(common_edge)
-    # if direction_in_other == 'w' and not rev:
-    #     continue
-    if direction_in_other == "n" and rev:
-        next_tile.flip_x()
-    elif direction_in_other == "e" and not rev:
-        next_tile.rotate90()
-        next_tile.rotate90()
-        next_tile.rotate90()
-    elif direction_in_other == "e" and rev:
-        next_tile.flip_y()
-        next_tile.rotate90()
-        next_tile.rotate90()
-        next_tile.rotate90()
-    elif direction_in_other == "w" and not rev:
-        next_tile.rotate90()
-    elif direction_in_other == "w" and rev:
-        next_tile.flip_y()
-        next_tile.rotate90()
-    elif direction_in_other == "s" and not rev:
-        next_tile.flip_y()
-    elif direction_in_other == "s" and rev:
-        next_tile.rotate90()
-        next_tile.rotate90()
-    return next_tile
-
-
-# dir_map_rotation = {
-#     'e': ['w', 's', 'e', 'n'],
-#     's': ['n', 'w', 's', 'e'],
-#     'w': ['e', 'n', 'w', 's'],
-#     'n': ['s', 'e', 'n', 'w'],
-# }
-dir_map_rotation = {
-    'e': ['w', 'n', 'e', 's'],
-    's': ['n', 'e', 's', 'w'],
-    'w': ['e', 's', 'w', 'n'],
-    'n': ['s', 'w', 'n', 'e'],
+map_rotations_to_match = {
+    "e": ["w", "n", "e", "s"],
+    "s": ["n", "e", "s", "w"],
+    "w": ["e", "s", "w", "n"],
+    "n": ["s", "w", "n", "e"],
 }
-dir_map_flip = {
-    'e': 'y',
-    's': 'x',
-    'w': 'y',
-    'n': 'x',
+map_flip_type = {
+    "e": "y",
+    "s": "x",
+    "w": "y",
+    "n": "x",
 }
-desired_side_map = {
-    'e': 'w',
-    's': 'n',
-    'w': 'e',
-    'n': 's',
+map_desired_side = {
+    "e": "w",
+    "s": "n",
+    "w": "e",
+    "n": "s",
 }
+
+map_next_tile_coords = {
+    "e": (1, 0),
+    "s": (0, -1),
+    "w": (-1, 0),
+    "n": (0, 1),
+}
+
+monster_xy_list = [
+    (0, 19),
+    (1, 0),
+    (1, 5),
+    (1, 6),
+    (1, 11),
+    (1, 12),
+    (1, 17),
+    (1, 18),
+    (1, 19),
+    (2, 1),
+    (2, 4),
+    (2, 7),
+    (2, 10),
+    (2, 13),
+    (2, 16),
+]
+
 
 def update_next_tiles(tile, tiles, edges_map, tiles_map, x, y):
     sides = tile.get_sides()
     tiles_map[(x, y)] = tile
     for direction, side in sides.items():
-        if edges_map[''.join(side)] == 1:
+        side_string = "".join(side)
+        if edges_map[side_string] == 1:
             continue
-        next_tile = [t for t in tiles if t.side_match(side) is not None and tile != t][0]
+        next_tile = next(
+            t for t in tiles if t.side_match(side) is not None and tile != t
+        )
         if next_tile in tiles_map.values():
             continue
-        direction_in_next, rev = next_tile.side_match(side)
-        rotation_count = dir_map_rotation[direction].index(direction_in_next)
+        direction_in_next = next_tile.side_match(side)
+        rotation_count = map_rotations_to_match[direction].index(direction_in_next)
         for _ in range(rotation_count):
             next_tile.rotate90()
 
-        desired_side_name = desired_side_map[direction]
-        other_side = next_tile.get_sides()[desired_side_name]
-        if ''.join(side) == ''.join(reversed(other_side)):
-            if dir_map_flip[direction] == 'x':
+        desired_side_name = map_desired_side[direction]
+        side_in_next = next_tile.get_sides()[desired_side_name]
+        need_to_flip = side_string == "".join(reversed(side_in_next))
+        if need_to_flip:
+            if map_flip_type[direction] == "x":
                 next_tile.flip_x()
             else:
                 next_tile.flip_y()
-        # elif dir_map_flip_when[direction] == direction_in_next:
-        #     if dir_map_flip[direction] == 'y':
-        #         next_tile.flip_x()
-        #     else:
-        #         next_tile.flip_y()
 
-
-        if direction == 'e':
-            update_next_tiles(next_tile, tiles, edges_map, tiles_map, x + 1, y)
-        if direction == 's':
-            update_next_tiles(next_tile, tiles, edges_map, tiles_map, x, y - 1)
-        if direction == 'w':
-            update_next_tiles(next_tile, tiles, edges_map, tiles_map, x - 1, y)
-        if direction == 'n':
-            update_next_tiles(next_tile, tiles, edges_map, tiles_map, x, y + 1)
+        next_x_add, next_y_add = map_next_tile_coords[direction]
+        update_next_tiles(
+            next_tile, tiles, edges_map, tiles_map, x + next_x_add, y + next_y_add
+        )
     return tiles_map
 
 
+def check_sea_monster(x, y, map):
+    try:
+        found = all([map[y + b][x + a] == "#" for a, b in monster_xy_list])
+    except IndexError:
+        return False
+    return found
 
+def draw_monster(x, y, graphic_map):
+    for a, b in monster_xy_list:
+        graphic_map[y + b][x + a] = '00ff00'
 
+def find_monsters(big_map):
+    for flip_x in range(2):
+        for flip_y in range(2):
+            for rotation in range(4):
+                monsters = 0
+                for y, row in enumerate(big_map):
+                    for x in range(len(row)):
+                        monsters += check_sea_monster(x, y, big_map)
+                        # print(x, end="")
+                    # print()
+                if monsters:
+                    return monsters, big_map
+                big_map = np.rot90(big_map)
 
-
-
-# def build_tiles_map(tiles, edges_map):
-#     return tiles_map
-#
-#     # map_len = int(math.sqrt(len(tiles)))
-#     #
-#     # tiles_map = []
-#     # for y in range(map_len):
-#     #     print(y)
-#     #     if y == 0:
-#     #         first_tile = corners[3]
-#     #         north_side = first_tile.get_sides()['n']
-#     #         if edges_map[''.join(north_side)] != 1:
-#     #             first_tile.flip_y()
-#     #         west_side = first_tile.get_sides()['w']
-#     #         if edges_map[''.join(west_side)] != 1:
-#     #             first_tile.flip_x()
-#     #         row = [first_tile]
-#     #     else:
-#     #         last_tile = tiles_map[y - 1][0]
-#     #         row = [get_tile_down_of(last_tile, tiles)]
-#     #
-#     #     tiles_map.append(row)
-#     #     for x in range(map_len - 1):
-#     #         print('x', x)
-#     #         last_tile = tiles_map[-1][-1]
-#     #         next_tile = get_tile_right_of(last_tile, tiles)
-#     #         tiles_map[y].append(next_tile)
-#     # return tiles_map
-
+            # flip y
+            big_map = list(reversed(big_map))
+        # flip x
+        new_map = []
+        for r in big_map:
+            new_map.append(list(reversed(r)))
+        big_map = new_map
+    return 0, big_map
 
 def second(tiles):
     edges = get_edges_count_map(tiles)
@@ -292,18 +190,63 @@ def second(tiles):
     tiles_map = {}
     # build tiles map recursively
     update_next_tiles(corners[0], tiles, edges, tiles_map, 0, 0)
-    print(tiles_map)
-    # print(sorted(tiles_map.items()))
-    min_x = min(tiles_map.keys(), key=lambda t: t[0])[0]
-    max_x = max(tiles_map.keys(), key=lambda t: t[0])[0]
-    min_y = min(tiles_map.keys(), key=lambda t: t[1])[1]
-    max_y = max(tiles_map.keys(), key=lambda t: t[1])[1]
+    # normalize tiles
+    min_tiles_x = min(tiles_map.keys(), key=lambda t: t[0])[0]
+    min_tiles_y = min(tiles_map.keys(), key=lambda t: t[1])[1]
+    max_tiles_x = max(tiles_map.keys(), key=lambda t: t[0])[0]
+    max_tiles_y = max(tiles_map.keys(), key=lambda t: t[1])[1]
+    
+    if min_tiles_x < 0 or min_tiles_y < 0:
+        pass
 
-    print(len(tiles), len(tiles_map))
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            print(tiles_map[(x, y)], end='')
-        print()
+
+
+
+    max_tiles_x += 1
+    max_tiles_y += 1
+    # build one big map
+    len_tile_y = len(tiles[0].rows) -2
+    len_tile_x = len(tiles[0].rows[0]) - 2
+    big_map = np.full((max_tiles_y * len_tile_y, max_tiles_x * len_tile_x), "X")
+
+    for tile_y in range(0, max_tiles_y):
+        for tile_x in range(0, max_tiles_x):
+            tile = tiles_map[(tile_x, tile_y)]
+            # print(tile.id)
+            for idx_y, row in enumerate(tile.rows[1: -1]):
+                for idx_x, value in enumerate(row[1: -1]):
+                    final_y = idx_y + (max_tiles_y - 1 - tile_y) * len_tile_y
+                    final_x = idx_x + tile_x * len_tile_x
+                    big_map[final_y][final_x] = value
+                    # print(value, end='')
+                # print()
+
+
+    monsters, correct_map = find_monsters(big_map)
+    print(monsters)
+
+    # graphic_map = np.full((len_tiles_y * len_tile_y, len_tiles_x * len_tile_x), 'xxxxxx')
+    # for y, row in enumerate(correct_map):
+    #     for x in range(len(row)):
+    #         if graphic_map[y][x] == 'xxxxxx':
+    #             has_monster_tail = check_sea_monster(x, y, correct_map)
+    #             if big_map[y][x] == '#':
+    #                 graphic_map[y][x] = '2697bd'
+    #             else:
+    #                 graphic_map[y][x] = '52bfe3'
+    #
+    #             if has_monster_tail:
+    #                 draw_monster(x, y, graphic_map)
+    # as_rgb = []
+    # for row in graphic_map:
+    #     simple_list = []
+    #     for value in row:
+    #         simple_list.append([int(value[i:i+2], 16) for i in (0, 2, 4)])
+    #     as_rgb.append(simple_list)
+    #
+    # img = np.array(as_rgb, dtype=np.uint8)
+    # fig = px.imshow(img)
+    # fig.show()
 
 
 def main():
